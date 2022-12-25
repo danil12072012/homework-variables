2f75c5426b5ee63d548a2f70559afaf413f40242
mycomputer <Danil12.07.2012@gail.com>
2022-12-25 13:59:48 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        var box = 5;
-        System.out.println(box);
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
     }
 }
