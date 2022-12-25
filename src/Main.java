97a22822c40612aee46462715101c2456abc3edf
mycomputer <Danil12.07.2012@gail.com>
2022-12-25 15:56:43 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        var timepiece = 640;
-        var employees = timepiece / 8;
-        System.out.println("Всего работников в компании - " + employees + " человек.");
-        employees = employees + 94;
-        var timepiecePerEmployee = timepiece / employees;
-        System.out.println("Если в компании работает " + employees + " человек, то всего " + timepiecePerEmployee + " часов работы может быть поделено между сотрудниками.");
+        byte a = 56;
+        short b = 168;
+        int c = 5684425;
+        long d = 35463;
+        float e = 2;
+        double f = 2.024534124;
 
+        System.out.println("Значение переменной а с типом byte равно " + a);
+        System.out.println("Значение переменной b с типом short равно " + b);
+        System.out.println("Значение переменной c с типом int равно " + c);
+        System.out.println("Значение переменной d с типом long равно " +d );
+        System.out.println("Значение переменной e с типом float равно " + e);
+        System.out.println("Значение переменной f с типом double равно " + f);
 
     }
 }
